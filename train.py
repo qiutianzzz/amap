42d82a8932016d97908f2afa47c294977d256988
qiutianzzz <lingquan_zhang@163.com>
2020-08-23 22:14:50 +0800

    succeed

--- train.py
+++ train.py
@@ -39,116 +39,60 @@ image_transforms = {
     ])
 }
 
-def read_image(path, dtype=np.float32, color=True):
-    """Read an image from a file.
-
-    This function reads an image from given file. The image is CHW format and
-    the range of its value is :math:`[0, 255]`. If :obj:`color = True`, the
-    order of the channels is RGB.
-
-    Args:
-        path (str): A path of image file.
-        dtype: The type of array. The default value is :obj:`~numpy.float32`.
-        color (bool): This option determines the number of channels.
-            If :obj:`True`, the number of channels is three. In this case,
-            the order of the channels is RGB. This is the default behaviour.
-            If :obj:`False`, this function returns a grayscale image.
-
-    Returns:
-        ~numpy.ndarray: An image.
-    """
-
-    f = Image.open(path)
-    try:
-        if color:
-            img = f.convert('RGB')
-        else:
-            img = f.convert('P')
-        # img = np.asarray(img, dtype=dtype)
-    finally:
-        if hasattr(f, 'close'):
-            f.close()
-
-    # if img.ndim == 2:
-    #     # reshape (H, W) -> (1, H, W)
-    #     return img[np.newaxis]
-    # else:
-    #     # transpose (H, W, C) -> (C, H, W)
-    #     return img.transpose((2, 0, 1))
 
 class AMAPDataset:
-    def __init__(self, phase, transform=None):
+    def __init__(self, img_dir, transform=None):
         self.data_dir = data_dir
         self.label_names = AMAP_LABEL_NAMES
-        self.phase = phase
-        img_dir = ''
+        self.img_dir = img_dir
+        self.ids = os.listdir(self.data_dir + self.img_dir)
         json_file = ''
-        
-        self.ids = os.listdir(self.data_dir + img_dir)
         self.transform = transform
 
-    def __len__(self):
-        return len(self.ids)
-
     def get_example(self, i):
-
-        id_ = self.ids[i]
+        
         # image = []
         # label = 0
-        if self.phase == 'train':
-            img_dir = 'amap_traffic_train_0712'
+        if self.img_dir == 'amap_traffic_train_0712':
             json_file = "amap_traffic_annotations_train.json"
-        elif self.phase == 'valid':
-            img_dir = 'amap_traffic_test_0712'
+        elif self.img_dir == 'amap_traffic_test_0712':
             json_file = "amap_traffic_annotations_test.json"
 
+        
+        id_ = self.ids[i]
         with open(self.data_dir+json_file,"r") as f:
             content=f.read()
         content=json.loads(content)
         cid = content['annotations'][i]
     
         # if id_ == cid['id']:      
-        img_file = os.path.join(self.data_dir, img_dir, cid['id'], cid['key_frame'])
+        img_file = os.path.join(self.data_dir, self.img_dir, cid['id'], cid['key_frame'])
         # print(img_file)
         img = Image.open(img_file).convert('RGB')
         # img = read_image(img_file, color=True)
         img = self.transform(img)
         label = (cid['status'])+1
         return img, label
+
+    def __len__(self):
+        return len(self.ids)
     __getitem__ = get_example
 
-
-# train_directory = os.path.join(dataset, 'train')
-# valid_directory = os.path.join(dataset, 'valid')
  
 batch_size = 32
 num_classes = 3
- 
-# data = {
-#     'train': datasets.ImageFolder(root=train_directory, transform=image_transforms['train']),
-#     'valid': datasets.ImageFolder(root=valid_directory, transform=image_transforms['valid']) 
-# }
 
-
-# data_loaders = {'train': dataloader, 'val': test_dataloader}   
- 
-# train_data_size = len(data['train'])
-# valid_data_size = len(data['valid'])
- 
-# train_data = DataLoader(data['train'], batch_size=batch_size, shuffle=True)
-# valid_data = DataLoader(data['valid'], batch_size=batch_size, shuffle=True)
-
-
-train_dataset = AMAPDataset(phase = 'train',transform = image_transforms['train'])
-valid_dataset = AMAPDataset(phase = 'valid',transform = image_transforms['valid'])
+train_dataset = AMAPDataset(img_dir = 'amap_traffic_train_0712',transform = image_transforms['train'])
+valid_dataset = AMAPDataset(img_dir = 'amap_traffic_test_0712',transform = image_transforms['valid'])
 #设置batch size = 64
+print(train_dataset.__len__())
 
 train_data_size = len(train_dataset)
 valid_data_size = len(valid_dataset)
 
 train_data = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
 valid_data = DataLoader(dataset=valid_dataset)
- 
+print(train_data.__len__())
 
 resnet50 = models.resnet50(pretrained=True)
 
@@ -160,7 +104,7 @@ resnet50.fc = nn.Sequential(
     nn.Linear(fc_inputs, 256),
     nn.ReLU(),
     nn.Dropout(0.4),
-    nn.Linear(256, 3),
+    nn.Linear(256, 10),
     nn.LogSoftmax(dim=1)
 )
 
@@ -188,6 +132,7 @@ def train_and_valid(model, loss_function, optimizer, epochs=25):
         valid_acc = 0.0
  
         for i, (inputs, labels) in enumerate(train_data):
+            print(i)
             # inputs = inputs.to(device)
             inputs = Variable(inputs.cuda())
             labels = Variable(labels.cuda())
@@ -204,8 +149,7 @@ def train_and_valid(model, loss_function, optimizer, epochs=25):
             loss.backward()
  
             optimizer.step()
-            print(inputs.size(0))
- 
+            # print(inputs.size(0))
             train_loss += loss.item() * inputs.size(0)
  
             ret, predictions = torch.max(outputs.data, 1)
@@ -254,12 +198,12 @@ def train_and_valid(model, loss_function, optimizer, epochs=25):
         ))
         print("Best Accuracy for validation : {:.4f} at epoch {:03d}".format(best_acc, best_epoch))
  
-        torch.save(model, 'models/'+dataset+'_model_'+str(epoch+1)+'.pt')
+        torch.save(model, 'checkpoints/'+'_model_'+str(epoch+1)+'.pt')
     return model, history
 
 num_epochs = 3
 trained_model, history = train_and_valid(resnet50, loss_func, optimizer, num_epochs)
-torch.save(history, 'models/'+dataset+'_history.pt')
+torch.save(history, 'checkpoints/'+'_history.pt')
  
 history = np.array(history)
 plt.plot(history[:, 0:2])
@@ -267,13 +211,13 @@ plt.legend(['Tr Loss', 'Val Loss'])
 plt.xlabel('Epoch Number')
 plt.ylabel('Loss')
 plt.ylim(0, 1)
-plt.savefig(dataset+'_loss_curve.png')
-plt.show()
+plt.savefig('_loss_curve.png')
+# plt.show()
  
 plt.plot(history[:, 2:4])
 plt.legend(['Tr Accuracy', 'Val Accuracy'])
 plt.xlabel('Epoch Number')
 plt.ylabel('Accuracy')
 plt.ylim(0, 1)
-plt.savefig(dataset+'_accuracy_curve.png')
-plt.show()
+plt.savefig('_accuracy_curve.png')
+# plt.show()
